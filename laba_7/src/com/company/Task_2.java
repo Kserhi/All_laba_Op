0326636e955b3104733df826743d49ceeefacaa1
Kserhi <99317407+Kserhi@users.noreply.github.com>
2022-05-05 12:28:36 +0300

    Update Task_2.java

--- laba_7/src/com/company/Task_2.java
+++ laba_7/src/com/company/Task_2.java
@@ -2,11 +2,11 @@ package com.company;
 
 public class Task_2 {
     public static void main(String[] args) {
-        double a=3,b=4,S=(a*b)/2,k,P;
+        double a=3,b=4,s=(a*b)/2,k,p;
         k=Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
-        P=a+b+k;
-        System.out.println("периметер:"+P);
-        System.out.println("площа:"+S);
+        p=a+b+k;
+        System.out.println("периметер:"+p);
+        System.out.println("площа:"+s);
 
     }
 }
